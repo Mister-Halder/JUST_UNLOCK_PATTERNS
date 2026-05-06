c8e8885fae9398c303b2c73e4ebf5771588b86ea
Debashis Halder <debashishalder185@gmail.com>
2026-05-07 00:36:41 +0530

    Add files via upload

---
+++ BASIC_PATTERNS/RotatedHalfPyramidWithNumbersPattern.java
@@ -0,0 +1,16 @@
+public class RotatedHalfPyramidWithNumbersPattern {
+    public static void rotatedhalfpyramidwithNumbers(int n) {
+         for(int i=n; i>=1; i--) {
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        rotatedhalfpyramidwithNumbers(5);   
+    }
+}
